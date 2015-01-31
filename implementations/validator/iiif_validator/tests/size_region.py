66cc8a62ed057bd322345a9e117eff5bf7222b81
Simeon Warner <simeon.warner@cornell.edu>
2015-01-31 12:28:03 -0500

    Improve comment, tidy trailing whitespace

--- implementations/validator/iiif_validator/tests/size_region.py
+++ implementations/validator/iiif_validator/tests/size_region.py
@@ -10,7 +10,8 @@ class Test_Size_Region(BaseTest):
 
     def run(self, result):
         try:
-            # ask for a random region, at a random size < 100
+            # ask for a random region, at a random size < 100 so that
+            # it is within one color square of the test image
             for i in range(5):
                 s = random.randint(35,90)
                 x = random.randint(0,9)
@@ -19,10 +20,10 @@ class Test_Size_Region(BaseTest):
                 params['region'] = '%s,%s,100,100' % (x*100, y*100)
                 img = result.get_image(params)
                 if img.size != (s,s):
-                    raise ValidatorError('size', img.size, (s,s), result)        
+                    raise ValidatorError('size', img.size, (s,s), result)
                 ok = self.validationInfo.do_test_square(img,x,y, result)
                 if not ok:
-                    raise ValidatorError('color', 1, self.validationInfo.colorInfo[0][0], result)            
+                    raise ValidatorError('color', 1, self.validationInfo.colorInfo[0][0], result)
             return result
         except:
             self.validationInfo.check('status', result.last_status, 200, result)
