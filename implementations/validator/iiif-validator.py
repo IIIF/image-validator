2696f0223a8495b63f506f6df839bacd034b3e35
Rob Sanderson <azaroth42@gmail.com>
2015-02-06 14:48:35 -0500

    Fix for apache/mod_wsgi

--- implementations/validator/iiif-validator.py
+++ implementations/validator/iiif-validator.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-"""Minimal wrapper around iiif-validator/validator.py to run server
+"""Minimal wrapper around iiif-validator/validator.py to run server 
+   from the command line, or from mod_wsgi in apache
 """
-from iiif_validator.validator import main
-main()
+
+from iiif_validator.validator import main, apache
+if __name__ == "__main__":
+    main()
+else:
+    application = apache()
