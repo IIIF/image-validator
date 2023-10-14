2e42990bec944596acbcfec4f67b341c566e9110
Danny Berger <dpb587@gmail.com>
2023-10-14 14:29:40 -0600

    Support non-wildcard CORS response headers
    Wildcards are recommended for broadest access, but request-based origins
    are still valid.
    
    https://iiif.io/api/image/3.0/#71-cors
    https://www.w3.org/TR/cors/

--- iiif_validator/validator.py
+++ iiif_validator/validator.py
@@ -255,9 +255,12 @@ class ImageAPI(object):
                 return uri
         return None
 
+    def get_request_origin(self):
+        return "http://iiif.io"
+
     def fetch(self, url):
         # Make it look like a real browser request
-        HEADERS = {"Origin": "http://iiif.io",
+        HEADERS = {"Origin": self.get_request_origin(),
             "Referer": "http://iiif.io/api/image/validator",
             "User-Agent": "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.1b3pre) Gecko/20081130 Minefield/3.1b3pre"}
         req = Request(url, headers=HEADERS)
